925b21f7a3d1f5f16a73afe1a8d084cf8c9fde9c
Chris Fordham <chris@xhost.com.au>
2011-12-01 20:06:18 +1100

    Add comment header.

--- bin/cs.rb
+++ bin/cs.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/ruby
 
 # chef-solo-wrapper (cs)
+#
+# Copyright 2011, Chris Fordham
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 CHEF_SOLO_WRAPPER_VERSION = '0.0.1'
 
@@ -30,6 +44,7 @@ EOS
   opt :help, 	  "Print usage info and exit.",                                       	  :short => "-h"
 end
 puts "options: #{opts.to_json}" unless !(opts.verbose || opts.debug and !opts.help)
+puts 'chef-solo-wrapper '+CHEF_SOLO_WRAPPER_VERSION
 
 solo = false
 server = false
