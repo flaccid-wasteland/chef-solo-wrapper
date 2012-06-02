98ec01df5fc07a658d3e2a42efa1034ac8b75cf1
Chris Fordham <chris@xhost.com.au>
2012-06-03 04:48:19 +1000

    Add same json dep as chef in gemspec.

--- chef-solo-wrapper.gemspec.rb
+++ chef-solo-wrapper.gemspec.rb
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'chef-solo-wrapper'
-  s.version     = '0.0.4'
+  s.version     = '0.0.5'
   s.date        = '2012-06-03'
   s.summary     = "chef-solo-wrapper"
   s.description = "A basic wrapper for chef-solo with RightScale integration."
@@ -9,6 +9,6 @@ Gem::Specification.new do |s|
   s.files       = [ "lib/config_helper.rb", "lib/cookbooks_fetcher.rb" ]
   s.executables << 'cs'
   s.homepage    = 'https://github.com/flaccid/chef-solo-wrapper'
-  s.add_dependency 'json'
+  s.add_dependency "json", ">= 1.4.4", "<= 1.6.1"
   s.add_dependency 'trollop'
 end
