6d6eda2f24bf33e603c2556ffb4b8f5d5b0b732e
Chris Fordham <chris@xhost.com.au>
2012-06-03 06:55:10 +1000

    Improve attribute merging and saving.

--- chef-solo-wrapper.gemspec.rb
+++ chef-solo-wrapper.gemspec.rb
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'chef-solo-wrapper'
-  s.version     = '0.0.5'
+  s.version     = '0.0.6'
   s.date        = '2012-06-03'
   s.summary     = "chef-solo-wrapper"
   s.description = "A basic wrapper for chef-solo with RightScale integration."
