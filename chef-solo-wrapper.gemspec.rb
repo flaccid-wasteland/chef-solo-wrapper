e7de41b54cf06f1207909185e237b3ce2fc76b09
Chris Fordham <chris@xhost.com.au>
2012-06-03 00:58:00 +1000

    Add json dep to gemspec.

--- chef-solo-wrapper.gemspec.rb
+++ chef-solo-wrapper.gemspec.rb
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'chef-solo-wrapper'
-  s.version     = '0.0.2'
-  s.date        = '2012-05-29'
+  s.version     = '0.0.3'
+  s.date        = '2012-06-02'
   s.summary     = "chef-solo-wrapper"
   s.description = "A basic wrapper for chef-solo with RightScale integration."
   s.authors     = ["Chris Fordham"]
   s.email       = 'chris@xhost.com.au'
   s.executables << 'cs'
-  s.homepage    =
-    'https://github.com/flaccid/chef-solo-wrapper'
+  s.homepage    = 'https://github.com/flaccid/chef-solo-wrapper'
+  s.add_dependency 'json'
 end
