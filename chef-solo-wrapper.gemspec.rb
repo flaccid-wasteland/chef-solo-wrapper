fbd1839fa8b14be6829a4e440cbcf845504b9e70
Chris Fordham <chris@xhost.com.au>
2012-06-04 01:54:59 +1000

    Fix for 0.0.10 release.

--- chef-solo-wrapper.gemspec.rb
+++ chef-solo-wrapper.gemspec.rb
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'chef-solo-wrapper'
-  s.version     = '0.0.9'
+  s.version     = '0.0.10'
   s.date        = '2012-06-04'
   s.summary     = "chef-solo-wrapper"
   s.description = "A basic wrapper for chef-solo with RightScale integration."
   s.authors     = ["Chris Fordham"]
   s.email       = 'chris@xhost.com.au'
-  s.files       = [ "lib/config_helper.rb", "lib/cookbooks_fetcher.rb" ]
+  s.files       = [ "lib/chef_solo_wrapper.rb", "lib/config_helper.rb", "lib/easy_logger.rb", "lib/cookbooks_fetcher.rb" ]
   s.executables << 'cs'
   s.homepage    = 'https://github.com/flaccid/chef-solo-wrapper'
   s.add_dependency "json", ">= 1.4.4", "<= 1.6.1"
